838eeddfc961c58bd545c1988e05168e8ad51d74
Ryan Leonard <rhinoisnot@gmail.com>
2018-01-05 13:05:49 -0500

    Added missing classes to 'index.js'.

--- index.js
+++ index.js
@@ -4,12 +4,48 @@ module.exports = class ArbitraryGitHubWebhooks {
     return require("data/GitHubID");
   }
 
+  static get GitHubTime() {
+    return require("data/GitHubTime");
+  }
+
+  static get GitHubUsername() {
+    return require("data/GitHubUsername");
+  }
+
+  static get WebhookIssue() {
+    return require("data/WebhookIssue");
+  }
+
   static get WebhookLabel() {
     return require("data/WebhookLabel");
   }
 
   static get WebhookLabelList() {
     return require("data/WebhookLabelList");
+  }
+
+  static get WebhookMilestone() {
+    return require("data/WebhookMilestone");
+  }
+
+  static get WebhookOrganization() {
+    return require("data/WebhookOrganization");
+  }
+
+  static get WebhookRepository() {
+    return require("data/WebhookRepository");
+  }
+
+  static get WebhookUser() {
+    return require("data/WebhookUser");
+  }
+
+  static get ArbitraryGitHubWebhook() {
+    return require("data/ArbitraryGitHubWebhook");
+  }
+
+  static get ArbitraryIssuesEvent() {
+    return require("data/ArbitraryIssuesEvent");
   }
 
 };
