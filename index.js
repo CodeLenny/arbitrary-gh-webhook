a5385508b884109d33e587a374e294a0adf5fa7c
Ryan Leonard <rhinoisnot@gmail.com>
2018-01-07 12:27:07 -0500

    Fixed paths.

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 module.exports = class ArbitraryGitHubWebhooks {
 
   static get GitHubID() {
-    return require("data/GitHubID");
+    return require("./data/GitHubID");
   }
 
   static get GitHubTime() {
-    return require("data/GitHubTime");
+    return require("./data/GitHubTime");
   }
 
   static get GitHubUsername() {
-    return require("data/GitHubUsername");
+    return require("./data/GitHubUsername");
   }
 
   static get WebhookIssue() {
-    return require("data/WebhookIssue");
+    return require("./data/WebhookIssue");
   }
 
   static get WebhookLabel() {
-    return require("data/WebhookLabel");
+    return require("./data/WebhookLabel");
   }
 
   static get WebhookLabelList() {
-    return require("data/WebhookLabelList");
+    return require("./data/WebhookLabelList");
   }
 
   static get WebhookMilestone() {
-    return require("data/WebhookMilestone");
+    return require("./data/WebhookMilestone");
   }
 
   static get WebhookOrganization() {
-    return require("data/WebhookOrganization");
+    return require("./data/WebhookOrganization");
   }
 
   static get WebhookRepository() {
-    return require("data/WebhookRepository");
+    return require("./data/WebhookRepository");
   }
 
   static get WebhookUser() {
-    return require("data/WebhookUser");
+    return require("./data/WebhookUser");
   }
 
   static get ArbitraryGitHubWebhook() {
-    return require("data/ArbitraryGitHubWebhook");
+    return require("./hooks/ArbitraryGitHubWebhook");
   }
 
   static get ArbitraryIssuesEvent() {
-    return require("data/ArbitraryIssuesEvent");
+    return require("./hooks/ArbitraryIssuesEvent");
   }
 
 };
